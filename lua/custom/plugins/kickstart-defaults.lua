79d969e9ade855ac7b70cbab1d54fba05095a11f
zackaj <aktouf98zakaria@gmail.com>
2024-06-04 22:54:05 +0100

    changed theme

--- lua/custom/plugins/kickstart-defaults.lua
+++ lua/custom/plugins/kickstart-defaults.lua
@@ -213,7 +213,7 @@ return {
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("cyberdream")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
