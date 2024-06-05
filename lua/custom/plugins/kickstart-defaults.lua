128cfc6d8329a57229e66c82cea84ef5fc7378c6
zackaj <aktouf98zakaria@gmail.com>
2024-06-05 20:58:08 +0100

    gitsigns configuration

--- lua/custom/plugins/kickstart-defaults.lua
+++ lua/custom/plugins/kickstart-defaults.lua
@@ -19,18 +19,6 @@ return {
 	--    require('gitsigns').setup({ ... })
 	--
 	-- See `:help gitsigns` to understand what the configuration keys do
-	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
 
 	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
 	--
--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -2,7 +2,20 @@ return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	opts = {
-		ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+		ensure_installed = {
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"vim",
+			"vimdoc",
+			"php",
+			"javascript",
+			"html",
+		},
 		-- Autoinstall languages that are not installed
 		auto_install = true,
 		highlight = {
