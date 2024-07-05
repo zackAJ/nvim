4f070e4653db3d7cd8b7a93b50f96f207915f778
zackaj <aktouf98zakaria@gmail.com>
2024-07-05 01:42:21 +0100

    added notifications and promps ui and fixed telescope colors

---
+++ lua/custom/plugins/ui-and-notifications.lua
@@ -0,0 +1,53 @@
+local notificationIcon = "🔔 "
+
+return {
+	-- components
+	"folke/noice.nvim",
+	enabled = true,
+	event = "VeryLazy",
+	opts = {
+		routes = {
+			{
+				view = "cmdline",
+				-- view = "notify",
+				filter = { event = "msg_showmode" },
+			},
+		},
+		lsp = {
+			-- override markdown rendering so that
+			-- **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+		presets = {
+			bottom_search = false,
+			-- position the cmdline and popupmenu together
+			command_palette = false,
+			-- long messages will be sent to a split
+			long_message_to_split = true,
+			-- enables an input dialog for inc-rename.nvim
+			inc_rename = false,
+			-- add a border to hover docs and signature help
+			lsp_doc_border = false,
+		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		{
+			-- notifications
+			"rcarriga/nvim-notify",
+			keys = {
+				{
+					"<leader>dn",
+					function()
+						require("notify").dismiss({ silent = true, pending = true })
+					end,
+					desc = notificationIcon .. "Dismiss all Notifications",
+				},
+			},
+		},
+	},
+}
