d804ec8a1a8dab0608a2566424a09e661ae98dc2
zackaj <aktouf98zakaria@gmail.com>
2024-08-16 01:36:10 +0100

    removed param

--- lua/custom/plugins/git-stuff.lua
+++ lua/custom/plugins/git-stuff.lua
@@ -28,9 +28,6 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
 				virt_text_priority = 100,
 			},
 			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-			current_line_blame_formatter_opts = {
-				relative_time = false,
-			},
 			sign_priority = 6,
 			update_debounce = 100,
 			status_formatter = nil, -- Use default
